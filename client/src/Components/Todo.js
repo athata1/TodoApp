4f7fb7ef07c258bcbfac57ce6ba83d401db72b02
Akhil Thata <akhil.thata@gmail.com>
2022-12-24 16:18:20 -0500

    Fixed key

--- client/src/Components/Todo.js
+++ client/src/Components/Todo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Todo({todoKey ,todo}) {
+export default function Todo({todo}) {
 
   return (
     <div className='todo-item'>
       <label className='todo-text'>
         <input type="checkbox" checked={todo.complete}/>
-        <span>{todo.name}</span>
+        <span> {todo.name}</span>
       </label>
     </div>
   )
